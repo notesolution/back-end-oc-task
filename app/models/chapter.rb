c835af014d780a0be1f8742eed883b89233bab70
Ted Cai <ted.xmdgj@gmail.com>
2018-08-19 14:10:17 +0800

    generate random edges

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -27,13 +27,18 @@ class Chapter < ApplicationRecord
     order = if max_room == 2
               [1, 2]
             else
-              shuffled_order = (2..(max_room - 1)).to_a.shuffle
-              [1] + shuffled_order + [max_room]
+              [1] + (2..(max_room - 1)).to_a.shuffle + [max_room]
             end
 
     edges = {}
     order.each_with_index do |room_number, index|
-      edges[room_number] = [order&.[](index+1)].compact
+      edges[room_number] = if room_number == max_room
+                             [order&.[](index+1)].compact
+                           else
+                             random_pick_count = rand(1..max_room - 2)
+                             random_edges = ((1..(max_room - 1)).to_a - [room_number]).sample(random_pick_count)
+                             ([order&.[](index+1)] + random_edges).compact.uniq
+                           end
     end
 
     edges.each do |parent_room_number, values|
@@ -41,8 +46,7 @@ class Chapter < ApplicationRecord
         parent_room = new_room_list[parent_room_number.to_i - 1]
         child_room  = new_room_list[child_room_number.to_i - 1]
     
-        self.edges.build parent_room: parent_room,
-                         child_room: child_room
+        self.edges.build parent_room: parent_room, child_room: child_room
       end
     end
   end
